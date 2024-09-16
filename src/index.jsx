72f1bc24fc995aacb3f2fdeebb6a9ddfbaad837c
Tim Baines <timbaines@timbaines.com>
2024-09-16 17:28:52 -0400

    Updated navbar and index.jsx for Host component and siblings

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,8 @@ import VanDetail from './pages/VanDetail';
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
+import HostVans from "./pages/Host/HostVans";
+import HostVanDetail from "./pages/Host/HostVanDetail.jsx";
 import Layout from './components/Layout';
 import HostLayout from "./components/HostLayout";
 import './index.css';
@@ -17,20 +19,18 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<Layout />}>
-                    <Route path="/" element={<Home />} />
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
                     <Route path="about" element={<About />} />
-
-                    {/* practice for nested routes. This is not best practice for development */}
-                    <Route path="vans">
-                        <Route index element={<Vans />} />
-                        <Route path=":id" element={<VanDetail />} />
-                    </Route>
+                    <Route path="vans" element={<Vans />} />
+                    <Route path="vans/:id" element={<VanDetail />} />
 
                     <Route path="host" element={<HostLayout />}>
                         <Route index element={<Dashboard />} />
                         <Route path="income" element={<Income />} />
                         <Route path="reviews" element={<Reviews />} />
+                        <Route path="vans" element={<HostVans />} />
+                        <Route path="vans/:id" element={<HostVanDetail />} />
                     </Route>
                 </Route>
             </Routes>
