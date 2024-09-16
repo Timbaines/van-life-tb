65c695043697e0cdd27cb1e88e4347d1404f0dc7
Tim Baines <timbaines@timbaines.com>
2024-09-16 17:28:20 -0400

    Routing and pages for Host components

---
+++ src/pages/Host/HostVanDetail.jsx
@@ -0,0 +1,7 @@
+export default function HostVanDetail() {
+    return (
+        <div>
+            <h1>HostVanDetail Page</h1>
+        </div>
+    )
+}
---
+++ src/pages/Host/HostVans.jsx
@@ -0,0 +1,7 @@
+export default function HostVans() {
+    return (
+        <div>
+            <h1>HostVans Page</h1>
+        </div>
+    )
+}
