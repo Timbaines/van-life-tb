c52472a4468a0998dfb25474484b9b10c22fa81f
Tim Baines <timbaines@timbaines.com>
2024-09-16 23:55:26 -0400

    Search/Query Parameters for van type filter - useSearchParams

--- src/pages/Host/HostVanInfo.jsx
+++ src/pages/Host/HostVanInfo.jsx
@@ -4,11 +4,11 @@ export default function HostVanInfo() {
     const { currentVan } = useOutletContext()
 
     return (
-        <section>
+        <div>
             <h4>Name: {currentVan.name}</h4>
             <h4>Category: {currentVan.type}</h4>
             <h4>Description: {currentVan.description}</h4>
             <h4>Visibility: public</h4>
-        </section>
+        </div>
     )
 }
