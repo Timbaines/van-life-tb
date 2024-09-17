e9ef019e7f0acd11a15edc44522d2a0e6995b90c
Tim Baines <timbaines@timbaines.com>
2024-09-17 02:43:03 -0400

    Added AuthRequired.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -16,6 +16,7 @@ import HostVanPricing from "./pages/Host/HostVanPricing"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import Layout from './components/Layout';
 import HostLayout from "./components/HostLayout";
+import AuthRequired from "./components/AuthRequired";
 import './index.css';
 
 import './server';
@@ -34,17 +35,20 @@ export default function App() {
                         element={<Login />}
                     />
 
-                    <Route path="host" element={<HostLayout />}>
-                        <Route index element={<Dashboard />} />
-                        <Route path="income" element={<Income />} />
-                        <Route path="reviews" element={<Reviews />} />
-                        <Route path="vans" element={<HostVans />} />
-                        <Route path="vans/:id" element={<HostVanDetail />}>
-                            <Route index element={<HostVanInfo />} />
-                            <Route path="pricing" element={<HostVanPricing />} />
-                            <Route path="photos" element={<HostVanPhotos />} />
+                    <Route element={<AuthRequired />}>
+                        <Route path="host" element={<HostLayout />}>
+                            <Route index element={<Dashboard />} />
+                            <Route path="income" element={<Income />} />
+                            <Route path="reviews" element={<Reviews />} />
+                            <Route path="vans" element={<HostVans />} />
+                            <Route path="vans/:id" element={<HostVanDetail />}>
+                                <Route index element={<HostVanInfo />} />
+                                <Route path="pricing" element={<HostVanPricing />} />
+                                <Route path="photos" element={<HostVanPhotos />} />
+                            </Route>
                         </Route>
                     </Route>
+
                     <Route path="*" element={<NotFound />} />
                 </Route>
             </Routes>
