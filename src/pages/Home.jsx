08d109fa7920e41126f6b12bdeb3f09aa671133d
Tim Baines <timbaines@timbaines.com>
2024-09-20 09:47:53 -0400

    Layout Redesign and styling updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Home() {
     return (
         <section className="section-hero">
-            <div className="row">
+            <div className="container">
                 <h1 className="heading heading-primary">You got the travel plans, we got the travel vans.</h1>
                 <p className="hero-content">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your
                     perfect
